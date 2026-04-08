63a913f535078edf4f028e9a02415566c7260231
Prototypestore <dikeledireeks@outlook.com>
2026-04-08 20:39:00 +0200

    Update contact.js

--- contact.js
+++ contact.js
@@ -5,12 +5,21 @@ form.addEventListener('submit', (e) => {
   e.preventDefault(); // stop the default submit
 
   // collect form data
-  const formData = new FormData(form);
+  const formData = {
+    name: form.name.value,
+    email: form.email.value,
+    phone: form.phone.value,
+    service: form.service.value,
+    message: form.message.value
+  };
 
   // send to Google Apps Script
-  fetch('https://script.google.com/macros/s/AKfycbybMkX7srCoofPI0yJOSq-97JiTn6ResnyyJBt0_CQkXBRl6HmGSD9QWGinOdJomOMWGQ/exec', { // replace with your web app URL
+  fetch('https://script.google.com/macros/s/AKfycbybMkX7srCoofPI0yJOSq-97JiTn6ResnyyJBt0_CQkXBRl6HmGSD9QWGinOdJomOMWGQ/exec', { // your web app URL
     method: 'POST',
-    body: formData
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
   })
   .then(response => {
     if (response.ok) {
