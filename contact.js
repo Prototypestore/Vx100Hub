e172f69448d63fadc17894e37b72e3d933ad1cbf
Prototypestore <dikeledireeks@outlook.com>
2026-04-11 23:39:55 +0200

    Update contact.js

--- contact.js
+++ contact.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', (e) => {
 
   const formData = new FormData(form);
 
-  fetch('https://script.google.com/macros/s/AKfycby3aHJ0Kl-sJdu-Wj4QTJmBEpwMdOFRelAtQpZZ1BtskUZanoXoE65qe8DdTJ7JEABx9w/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbx4H4FuA2aepipxCU4xlfd7F6rLcvwTAHlDDdn8L_S2ZOA6cR9cRt2oLDuZ5JcIE0Nt/exec', {
     method: 'POST',
     body: formData
   })
